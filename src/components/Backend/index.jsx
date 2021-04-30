6e4721e946da18d8de905bcaed8f471ca37305a7
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-29 21:09:19 -0400

    backend

--- src/components/Backend/index.jsx
+++ src/components/Backend/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Project from "../../Projects";
+import Container from "../Container"
 
 function Backend() {
 
+  const projects = Project.filter(project => project.categories.includes("backEnd"))
+
   return (
     <>
-      <div>Test Back</div>
+      {projects.map(back => (
+        <Container key={back.name} item={back} />
+      ))}
     </>
   )
 }
