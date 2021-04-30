f7d9767ae67a50e0b0298f32630d00e0290ec872
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-29 21:10:36 -0400

    api

--- src/components/API/index.jsx
+++ src/components/API/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Project from "../../Projects";
+import Container from "../Container";
 
 function API() {
 
+  const projects = Project.filter(project => project.categories.includes("API"))
+
   return (
     <>
-      <div>Test API</div>
+      {projects.map(api => (
+        <Container key={api.name} item={api} />
+      ))}
     </>
   )
 }
