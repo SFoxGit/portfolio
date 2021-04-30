e5d0ec8a101a9a0e7e491e2f994d37b004ce4d0e
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-30 14:11:23 -0400

    home content

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -4,7 +4,12 @@ function Homepage() {
 
   return (
     <>
-      <div>Test</div>
+      <div>
+        <p>Welcome to the portfolio page for Shawn Fox.</p>
+        <p>Select a category within the Work tab in the navigation bar to view projects I have worked on.</p> 
+        <p>The About Me section gives a glipse into my background.</p>
+        <p> Most importantly, be sure to click the Contact tab to get in touch with me. Thanks for viewing! </p>
+      </div>
     </>
   )
 }
