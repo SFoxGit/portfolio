f323924bf5a224450b14ddd4543579c0d6703ca8
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-29 21:12:21 -0400

    react

--- src/components/React/index.jsx
+++ src/components/React/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Project from "../../Projects";
+import Container from "../Container";
 
 function Reactpage() {
 
+  const projects = Project.filter(project => project.categories.includes("React"))
+
   return (
     <>
-      <div>Test React</div>
+      {projects.map(react => (
+        <Container key={react.name} item={react} />
+      ))}
     </>
   )
 }
