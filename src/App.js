967a289034f32d99cf137cfaba3dd2923f653ba7
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-30 16:31:00 -0400

    particles

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import API from './components/API';
 import ReactPage from './components/React';
 import Group from './components/Group';
 import Contact from './components/Contact';
+import Particles from "react-particles-js";
 
 
 
@@ -18,33 +19,97 @@ function App() {
   return (
     <Router>
       <Header />
-      <div className="container">
-        <Switch>
-          <Route exact path="/">
-            <Homepage />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/frontend">
-            <FrontEnd />
-          </Route>
-          <Route exact path="/backend">
-            <BackEnd />
-          </Route>
-          <Route exact path="/api">
-            <API />
-          </Route>
-          <Route exact path="/react">
-            <ReactPage />
-          </Route>
-          <Route exact path="/group">
-            <Group />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
+      <div className="main-content">
+        <div className="parts">
+          <Particles
+            // height={window.outerHeight}
+            className="particles"
+            params={{
+              "particles": {
+                "number": {
+                  "value": 15,
+                  "density": {
+                    "enable": true,
+                    "value_area": 1800
+                  }
+                },
+                "line_linked": {
+                  "enable": false
+                },
+                "move": {
+                  "speed": 1,
+                  "out_mode": "out"
+                },
+                "shape": {
+                  "type": [
+                    "image",
+                    "image",
+                    "image",
+                  ],
+                  "image": [
+                    {
+                      "src": "/images/nodejs.png",
+                      "height": 20,
+                      "width": 20
+                    },
+                    {
+                      "src": "/images/jslogo.png",
+                      "height": 20,
+                      "width": 20
+                    },
+                    {
+                      "src": "/images/reactlogo.png",
+                      "height": 20,
+                      "width": 20
+                    }
+                  ]
+                },
+                "color": {
+                  "value": "#CCC"
+                },
+                "size": {
+                  "value": 30,
+                  "random": false,
+                  "anim": {
+                    "enable": true,
+                    "speed": 4,
+                    "size_min": 10,
+                    "sync": false
+                  }
+                }
+              },
+              "retina_detect": false
+            }} />
+        </div>
+        <div className="container">
+
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/frontend">
+              <FrontEnd />
+            </Route>
+            <Route exact path="/backend">
+              <BackEnd />
+            </Route>
+            <Route exact path="/api">
+              <API />
+            </Route>
+            <Route exact path="/react">
+              <ReactPage />
+            </Route>
+            <Route exact path="/group">
+              <Group />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
