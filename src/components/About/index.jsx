2c1985a65e9dc8c571cc4e167dbc4d8976758ccf
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-30 14:30:12 -0400

    about me content

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,7 +4,13 @@ function About() {
 
   return (
     <>
-      <div>Test About</div>
+      <div>
+        <p>My name is Shawn Fox, I currently reside in the Charlotte NC area but am open to relocation.</p>
+        <p>I have over a decade of very successful experience in the sales world. I tackled a wide variety of industries in that time ranging from the Oil and Gas industry, to cars, even to vinyl records.</p>
+        <p>In 2021 I decided to seek a career change into something I could be passionate about. Coding drew me in thanks to it primarly being logic based. It seemed less like work and more like playing constant logic based puzzle games. Once I got passed HTML and CSS and into Javascript, I was hooked. This is just plain fun to me, and I can't get enough of it.</p>
+        <p> There is an old mindset that dates back millennia, if you want to conquer a hostile place you burn your boats upon landing. There is no retreat, you conquer or you perish. I committed to learning to code, I am past the point of no return. I will conquer because I will not allow myself to perish.</p>
+        <p>I could have easily taken a part time coding bootcamp and continued to have my sales career to fall back on. I chose to instead do a full time course, cramming what some have described as 2-3 years worth of coding experience into 12 weeks. Fully committing to it without a fall back was my boat burning. I easily spent 90+ hours a week coding and learning during the 12 week course. There was hardly a waking moment that wasn't spent on code. I think if you look at the progress I've made in 12 weeks you'd be hard pressed to find someone that has picked up coding faster. To me, it ranks atop my accomplishments even above selling over $420,000 in PDC drill bits in a single month.</p>
+      </div>
     </>
   )
 }
