27dd51edcd0ed6c33110129f28e094307b01a807
Shawn Fox <foxeasttrojan06@gmail.com>
2021-04-30 20:13:04 -0400

    home style

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -4,11 +4,15 @@ function Homepage() {
 
   return (
     <>
-      <div>
-        <p>Welcome to the portfolio page for Shawn Fox.</p>
-        <p>Select a category within the Work tab in the navigation bar to view projects I have worked on.</p> 
-        <p>The About Me section gives a glipse into my background.</p>
-        <p> Most importantly, be sure to click the Contact tab to get in touch with me. Thanks for viewing! </p>
+      <div className="row border rounded mt-5 indCards p-3">
+        <div className="col-12 d-flex justify-content-center p-2">
+          <div className="row d-flex justify-content-center">
+            <p className="col-12">Welcome to the portfolio page for Shawn Fox.</p>
+            <p className="col-12">Select a category within the Work tab in the navigation bar to view projects I have worked on.</p>
+            <p className="col-12">The About Me section gives a glipse into my background.</p>
+            <p className="col-12"> Most importantly, be sure to click the Contact tab to get in touch with me. Thanks for viewing! </p>
+          </div>
+        </div>
       </div>
     </>
   )
