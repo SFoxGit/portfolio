013a47d3700b8b91b8cbd42d7a2316c5569945fd
Shawn Fox <foxeasttrojan06@gmail.com>
2021-05-05 16:27:23 -0400

    react sort/filter

--- src/components/React/index.jsx
+++ src/components/React/index.jsx
@@ -3,9 +3,67 @@ import Project from "../../Projects";
 import Container from "../Container";
 
 function Reactpage() {
-
   const [projects, setProjects] = useState([]);
+  const [display, setDisplay] = useState("Sort by oldest")
 
+  function sortAscending(proj) {
+    setProjects(proj.sort((a, b) => {
+      let fa = a.startDate;
+      let fb = b.startDate;
+      if (fa < fb) {
+        return 1
+      };
+      if (fb < fa) {
+        return -1
+      };
+      return null;
+    }))
+  }
+
+  function sortDescending(proj) {
+    setProjects(proj.sort((a, b) => {
+      let fa = a.startDate;
+      let fb = b.startDate;
+      if (fa < fb) {
+        return -1
+      };
+      if (fb < fa) {
+        return 1
+      };
+      return null;
+    }));
+  }
+
+  const sortAsc = (event) => {
+    event.preventDefault();
+    if (display === "Sort by oldest") {
+      sortDescending(projects)
+      setDisplay("Sort by newest")
+    } else {
+      sortAscending(projects)
+      setDisplay("Sort by oldest")
+    }
+  }
+  const soloP = async (event) => {
+    event.preventDefault();
+    const unsorted = Project.filter(project => project.categories.includes("React"));
+    const solo = unsorted.filter(project => !project.categories.includes("group"));
+    if (display === "Sort by oldest") {
+      sortAscending(solo)
+    } else {
+      sortDescending(solo)
+    }
+  }
+  const groupP = async (event) => {
+    event.preventDefault();
+    const unsorted = Project.filter(project => project.categories.includes("React"));
+    const solo = unsorted.filter(project => project.categories.includes("group"));
+    if (display === "Sort by oldest") {
+      sortAscending(solo)
+    } else {
+      sortDescending(solo)
+    }
+  }
   useEffect(() => {
     const unsorted = Project.filter(project => project.categories.includes("React"));
     const sorted = unsorted.sort((a, b) => {
@@ -26,6 +84,11 @@ function Reactpage() {
 
   return (
     <>
+      <div className="row border rounded m-3 indCards p-3 justify-content-around">
+        <button onClick={(e) => sortAsc(e)} type="click" className="btn">{display}</button>
+        <button onClick={(e) => soloP(e)} type="click" className="btn">Solo Projects</button>
+        <button onClick={(e) => groupP(e)} type="click" className="btn">Group Projects</button>
+      </div>
       {projects.map(react => (
         <Container key={react.name} item={react} />
       ))}
