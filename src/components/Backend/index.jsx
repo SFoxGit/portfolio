0130328e8e45e93ff2c2e0d6d7d451f3dda5355f
Shawn Fox <foxeasttrojan06@gmail.com>
2021-05-05 16:10:55 -0400

    backend cleanup

--- src/components/Backend/index.jsx
+++ src/components/Backend/index.jsx
@@ -36,36 +36,11 @@ function Backend() {
 
   const sortAsc = (event) => {
     event.preventDefault();
-    console.log("test")
-
     if (display === "Sort by oldest") {
-      console.log("descending")
-      const descending = projects.sort((a, b) => {
-        let fa = a.startDate;
-        let fb = b.startDate;
-        if (fa < fb) {
-          return -1
-        };
-        if (fb < fa) {
-          return 1
-        };
-        return null;
-      });
-      setProjects(descending);
+      sortDescending(projects)
       setDisplay("Sort by newest")
     } else {
-      const ascending = projects.sort((a, b) => {
-        let fa = a.startDate;
-        let fb = b.startDate;
-        if (fa < fb) {
-          return 1
-        };
-        if (fb < fa) {
-          return -1
-        };
-        return null;
-      });
-      setProjects(ascending);
+      sortAscending(projects)
       setDisplay("Sort by oldest")
     }
   }
@@ -109,7 +84,7 @@ function Backend() {
 
   return (
     <>
-      <div className="row border rounded m-3 indCards p-3">
+      <div className="row border rounded m-3 indCards p-3 justify-content-around">
         <button onClick={(e) => sortAsc(e)} type="click" className="btn">{display}</button>
         <button onClick={(e) => soloP(e)} type="click" className="btn">Solo Projects</button>
         <button onClick={(e) => groupP(e)} type="click" className="btn">Group Projects</button>
