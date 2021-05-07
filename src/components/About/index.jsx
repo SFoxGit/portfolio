81144068e384671f25b6c4333fd2f9f5293989ca
Shawn Fox <foxeasttrojan06@gmail.com>
2021-05-07 03:11:50 -0400

    about stats

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -7,11 +7,40 @@ function About() {
       <div className="row border rounded mt-5 indCards p-3">
         <div className="col-12 d-flex justify-content-center p-2">
           <div className="row d-flex">
-            <p>My name is Shawn Fox, I currently reside in the Charlotte NC area but am open to relocation.</p>
+            <p>My name is Shawn Fox, I am a full-stack web developer currently residing in the Charlotte NC area but am open to relocation.</p>
             <p>I have over a decade of very successful experience in the sales world. I tackled a wide variety of industries in that time ranging from the Oil and Gas industry, to cars, even to vinyl records.</p>
             <p>In 2021 I decided to seek a career change into something I could be passionate about. Coding drew me in thanks to it primarly being logic based. It seemed less like work and more like playing constant logic based puzzle games. Once I got passed HTML and CSS and into Javascript, I was hooked. This is just plain fun to me, and I can't get enough of it.</p>
-            <p> There is an old mindset that dates back millennia, if you want to conquer a hostile place you burn your boats upon landing. There is no retreat, you conquer or you perish. I committed to learning to code, I am past the point of no return. I will conquer because I will not allow myself to perish.</p>
-            <p>I could have easily taken a part time coding bootcamp and continued to have my sales career to fall back on. I chose to instead do a full time course, cramming what some have described as 2-3 years worth of coding experience into 12 weeks. Fully committing to it without a fall back was my boat burning. I easily spent 90+ hours a week coding and learning during the 12 week course. There was hardly a waking moment that wasn't spent on code. I think if you look at the progress I've made in 12 weeks you'd be hard pressed to find someone that has picked up coding faster. To me, it ranks atop my accomplishments even above selling over $420,000 in PDC drill bits in a single month.</p>
+
+          </div>
+        </div>
+      </div>
+      <div className="row border rounded mt-5 indCards p-3">
+        <div className="col-12 d-flex justify-content-center p-2">
+          <div className="row d-flex">
+            <p className="col-12 text-center">My Top Five Tools</p>
+            <div className="col-12 d-flex justify-content-between">
+              <img src="https://img.shields.io/badge/-JavaScript-black?style=for-the-badge&logo=javascript" alt="javascript" />
+              <img src="https://img.shields.io/badge/-Nodejs-black?style=for-the-badge&logo=Node.js" alt="node" />
+              <img src="https://img.shields.io/badge/-MySQL-black?style=for-the-badge&logo=mysql" alt="MySQL" />
+              <img src="https://img.shields.io/badge/-React-black?style=for-the-badge&logo=react" alt="react" />
+              <img src="https://img.shields.io/badge/-Bootstrap-563D7C?style=for-the-badge&logo=bootstrap" alt="bootstrap" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row border rounded mt-5 indCards p-3">
+        <div className="col-12 d-flex justify-content-center p-2">
+          <div className="row d-flex justify-content-center">
+            <p className="col-12 text-center">GitHub Stats</p>
+            <div className="col-xs-12 col-md-6 d-flex justify-content-center mt-2">
+              <img className="" src="https://github-readme-stats.vercel.app/api?username=sfoxgit&theme=blue-green" alt="github stats"></img>
+            </div>
+            <div className="col-xs-12 col-md-6 d-flex justify-content-center mt-2">
+              <img className="" src="https://github-readme-streak-stats.herokuapp.com/?user=sfoxgit&theme=blue-green" alt="github streak"></img>
+            </div>
+            <div className="col-12 d-flex justify-content-center mt-4">
+              <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=sfoxgit&theme=blue-green" alt="Top Langs" />
+            </div>
           </div>
         </div>
       </div>
